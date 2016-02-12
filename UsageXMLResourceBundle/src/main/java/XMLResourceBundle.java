908995f705172a53e64843eff551c62ae75b77f1
KIYONAGA Toshihisa <t.kiyonaga@gmail.com>
2016-02-13 04:53:23 +0900

    change facade API name. confuse?

---
+++ UsageXMLResourceBundle/src/main/java/XMLResourceBundle.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class XMLResourceBundle
+{
+	private static final XMLResourceBundleControl xmlctl = new XMLResourceBundleControl();
+
+	/**
+	 * XML形式のリソースバンドルを返す。
+	 * @param baseName
+	 * @return
+	 */
+	public static final ResourceBundle getBundle(String baseName)
+	{
+		return ResourceBundle.getBundle(baseName, xmlctl);
+	}
+
+	/**
+	 * XML形式のリソースバンドルを返す。指定ロケールの。
+	 * @param baseName
+	 * @param locale
+	 * @return
+	 */
+	public static final ResourceBundle getBundle(String baseName, Locale locale)
+	{
+		return ResourceBundle.getBundle(baseName, locale, xmlctl);
+	}
+
+}
