779cadd08ba294f6f177769639fc6632b0c111b7
KIYONAGA Toshihisa <t.kiyonaga@gmail.com>
2016-02-26 00:09:50 +0900

    test.

--- u2sts/src/test/java/com/example/U2stsApplicationTests.java
+++ u2sts/src/test/java/com/example/U2stsApplicationTests.java
@@ -2,17 +2,27 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = U2stsApplication.class)
 @WebAppConfiguration
-public class U2stsApplicationTests {
+public class U2stsApplicationTests
+{
+	private static final Logger logger = LoggerFactory.getLogger(U2stsApplicationTests.class);
+
+	@Autowired
+	private U2stsApplication main;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads()
+	{
+		logger.debug(main.mmm());
 	}
 
 }
