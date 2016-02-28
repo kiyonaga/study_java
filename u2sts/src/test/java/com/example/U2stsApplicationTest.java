d1a3b23f5982e94fa0e69c2ce3009db2b860f853
KIYONAGA Toshihisa <t.kiyonaga@gmail.com>
2016-02-28 23:06:15 +0900

    enable test on eclipse.

--- u2sts/src/test/java/com/example/U2stsApplicationTest.java
+++ u2sts/src/test/java/com/example/U2stsApplicationTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -7,20 +8,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = U2stsApplication.class)
-@WebAppConfiguration
-public class U2stsApplicationTests
+//@WebAppConfiguration
+public class U2stsApplicationTest
 {
-	private static final Logger logger = LoggerFactory.getLogger(U2stsApplicationTests.class);
+	private static final Logger logger = LoggerFactory.getLogger(U2stsApplicationTest.class);
 
 	@Autowired
 	private U2stsApplication main;
 
 	@Test
 	public void contextLoads()
+	{
+		logger.debug(main.mmm());
+		Assert.assertEquals("", true, true);
+	}
+
+	@Test
+	public void mmm()
 	{
 		logger.debug(main.mmm());
 	}
